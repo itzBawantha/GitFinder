e983e88f33d21b5414d08f32cb636336e5fa6c00
Bawantha Chandula <Bawantha_119001@dialog.dialoggsm.com>
2020-07-28 15:36:17 +0530

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var request = require('request');
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0var burp0_headers = {
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+var burp0_headers = {
     "Upgrade-Insecure-Requests": "1", 
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
